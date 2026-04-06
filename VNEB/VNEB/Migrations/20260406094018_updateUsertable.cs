205f6143c4bda2cb30e938b829fa783ad2932f34
nvp01507@gmail.com <nvp01507@gmail.com>
2026-04-06 16:40:51 +0700

    update employee avatar

---
+++ VNEB/VNEB/Migrations/20260406094018_updateUsertable.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VNEB.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateUsertable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AvatarPath",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: "admin-fixed-id-001",
+                column: "AvatarPath",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AvatarPath",
+                table: "Users");
+        }
+    }
+}
